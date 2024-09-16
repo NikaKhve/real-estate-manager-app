ccf4ead457bbe1719631847aae69d12d62281610
NikaKhve <nikolozlg2@gmail.com>
2024-09-17 01:02:12 +0400

    feat: retrieve listing item id for get api call

--- src/pages/ListingItem.jsx
+++ src/pages/ListingItem.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom";
+
 import classes from "./ListingItem.module.scss";
 
 const ListingItem = () => {
+  let { id } = useParams();
+  console.log(id, "LOCATION");
+
   return <div className={classes.container}>ListingItem</div>;
 };
 
