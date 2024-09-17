57e039a5cbd268b0efd878b6a61effc439275d2e
NikaKhve <nikolozlg2@gmail.com>
2024-09-18 00:48:53 +0400

    feat: add is rental radio inputs

--- src/pages/AddNewListing.jsx
+++ src/pages/AddNewListing.jsx
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
+import { Radio, Group } from "@mantine/core";
+
 import classes from "./AddNewListing.module.scss";
 
 const AddNewListing = () => {
-  return <div>AddNewListing</div>;
+  const [formData, setFormData] = useState({
+    is_rental: 0, // 0 for 'იყიდება', 1 for 'ქირავდება'
+  });
+
+  const handleOnRadioInputChange = (val) => {
+    setFormData((prev) => ({
+      ...prev,
+      is_rental: val,
+    }));
+  };
+
+  useEffect(() => {
+    console.log(formData, "FORM");
+  }, [formData]);
+
+  return (
+    <div className={classes.container}>
+      <p className={classes.heading}>ლისტინგის დამატება</p>
+      <section className={classes.formWrapper}>
+        <div className={classes.radioInputsWrapper}>
+          <Radio.Group
+            name="transactionType"
+            label="გარიგების ტიპი"
+            size="xs"
+            value={formData.is_rental.toString()}
+            onChange={handleOnRadioInputChange}
+            classNames={{ label: classes.radioLabel }}
+          >
+            <Group mt="xs">
+              <Radio
+                value="0"
+                label="იყიდება"
+                size="xs"
+                variant="outline"
+                color="black"
+                classNames={{ body: classes.radioInput }}
+              />
+              <Radio
+                value="1"
+                label="ქირავდება"
+                size="xs"
+                variant="outline"
+                color="black"
+              />
+            </Group>
+          </Radio.Group>
+        </div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </section>
+    </div>
+  );
 };
 
 export default AddNewListing;
